3abeaa5cf652e7137a417e50d16c10babbd59a8b
Renato Velazco <renato_velazco@usmp.pe>
2021-04-20 20:20:06 -0500

    lista de ciudades

---
+++ Models/Pokemon.cs
@@ -0,0 +1,13 @@
+namespace pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+
+         public string Maestro { get; set; }
+        public string Lugar { get; set; }
+
+    }
+}
